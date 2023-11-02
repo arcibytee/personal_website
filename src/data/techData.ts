e34cb46222ed51addea354e78d8c762e87a9cd05
Jhon Arciniegas <71716787+jhonarciniegas@users.noreply.github.com>
2023-11-02 18:33:27 -0500

    ✨ [FEAT] Add Stack section

---
+++ src/data/techData.ts
@@ -0,0 +1,30 @@
+type Technology = {
+    name: string;
+    icon: string;
+  };
+  
+  const techData: Technology[] = [
+    {
+      name: 'React',
+      icon: '../icons/react.svg',
+    },
+    {
+      name: 'TailwindCSS',
+      icon: '../icons/tailwindcss.svg',
+    },
+    {
+        name: 'JavaScript',
+        icon: '../icons/javascript.svg',
+      },
+      {
+        name: 'TypeScript',
+        icon: '../icons/typescript.svg',
+      },
+      {
+        name: 'Astro',
+        icon: '../icons/astro.svg',
+      },
+  ];
+  
+  export default techData;
+  
